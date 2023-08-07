a1a93830f63c3f07737101c0baa4d2c0e41a91cb
Confucius <alyssa.pidge@live.com.au>
2023-08-07 12:22:27 +0930

    added get routes to html files

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,33 @@
 //import Express.js
-const express = requier("express");
+const express = require("express");
 //initialize express.js
 const app = express();
+//to server path for files lcoated on server
+const path = require("path");
 //specify port for express.js to run on:
 //update for heroku:
 const PORT = 3001;
+
+//serve static files when requested from public dr
+app.use(express.static("public"));
+
+//get route for sending HTML file for first HTML page:
+//use path.join method to join paths dirname adn index.html together:
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+//route for get notes HTML file:
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(_dirname, "notes.html"));
+});
+
+// //?need a fallback route for this application??
+// app.get("*", (req, res) =>
+//   res.send(
+//     `Make a GET request using Insomnia to <a href="http://localhost:${PORT}">http://localhost:${PORT}</a>`
+//   )
+// );
 
 //listen method for incoming conections to PORT
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
