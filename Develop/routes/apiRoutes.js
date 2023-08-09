f0128d6fd9776afef866f8e9953fe2e01a574630
Confucius <alyssa.pidge@live.com.au>
2023-08-09 23:05:21 +0930

    installed ROUTER from Express and seperated route

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const fs = require("fs");
+const uniqid = require("uniqid");
+
+router.get("/api/notes/", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+//route for notes post
+router.post("/api/notes", (req, res) => {
+  //output message to web browser:
+  console.info(`${req.method} request received to add a new note`);
+
+  const { title, text } = req.body;
+  console.log({ title, text }, "route hit");
+
+  //check request has both titel and text:
+  if (title && text) {
+    const newTodo = {
+      title: title,
+      text: text,
+      id: uniqid(),
+    };
+    console.log(newTodo);
+
+    //read current file that new data being ápended'to:
+    const currentTodoString = JSON.parse(fs.readFileSync("./db/db.json"));
+    //make new array using spread syntax to merge arrays:
+
+    // const newTodoList = [...currentTodoString, newTodo];
+
+    currentTodoString.push(newTodo);
+
+    //write to the file:
+    fs.writeFileSync("./db/db.json", JSON.stringify(currentTodoString));
+    console.log(`New To-Do ${newTodo} has been written to JOSN file`);
+
+    //   const response = {
+    //     status: "success",
+    //     body: newTodo,
+    //   };
+
+    // console.log(response);
+    res.status(201).json(currentTodoString);
+  } else {
+    res
+      .status(500)
+      .send(
+        "Error in posting to do item, please enter both title and text fields"
+      );
+  }
+  //log request -body to console
+  console.log(req.body);
+});
+
+module.exports = router;
--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,9 @@ const express = require("express");
 //initialize express.js
 const app = express();
 //to server path for files lcoated on server
-const path = require("path");
+const homeRoutes = require("./routes/homeRoutes");
+//to read file - allows updating file with new to do entries;
+const apiRoutes = require("./routes/apiRoutes");
 //specify port for express.js to run on:
 //update for heroku:
 const PORT = 3001;
@@ -16,39 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 
 //serve static files when requested from public dr
 app.use(express.static("public"));
-
-//get route for sending HTML file for first HTML page:
-//use path.join method to join paths dirname adn index.html together:
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-//route for get notes HTML file:
-app.get("/notes", (req, res) => {
-  //   res.send("hey");
-  res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-//route for notes post
-app.post("/notes", (req, res) => {
-  //output message to web browser:
-  console.info(`${req.method} request received to add a new note`);
-  // Prepare a response object to send back to the client
-  let response;
-  // Check if there is anything in the response body
-  if (req.body && req.body.product) {
-    response = {
-      status: "success",
-      data: req.body,
-    };
-    res.status(201).json(response);
-  } else {
-    res.status(400).json("Request body must at least contain a product name");
-  }
-
-  //log request -body to console
-  console.log(res.body);
-});
+app.use(apiRoutes);
+app.use(homeRoutes);
 
 // //?need a fallback route for this application??
 // app.get("*", (req, res) =>
